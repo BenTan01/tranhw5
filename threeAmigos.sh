99bd422684a1cbfa3d763a6925bdd59be4209d29
Tan Tran <tandutran2001@gmail.com>
2022-09-29 19:46:01 +0000

    Completed Problem 2

---
+++ threeAmigos.sh
@@ -0,0 +1,10 @@
+# threeAmigos.sh take 4 command line arguments
+# All the arguments are number
+# e.g. ./threeAmigos.sh 5 5 5 4
+
+#!/bin/bash
+
+sum=$(($1+$2+$3))				# Calculate the sum of first three arguments
+echo Sum = $sum					# Print out the sum
+
+./subtractMachine.sh $sum $4			# Call subtractMachine.sh script
