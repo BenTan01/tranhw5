0843bfe6916dd01a75daa3f88d7a8fe37833a6c0
Tan Tran <tandutran2001@gmail.com>
2022-10-04 23:22:26 +0000

    Finish Problem 4

---
+++ commanderBash.sh
@@ -0,0 +1,19 @@
+# commanderBash.sh take no arguments
+# e.g. ./commanderBash.sh
+
+#!/bin/bash
+
+echo "Turning LED on"
+./led-hw5.sh on
+
+echo "Turning LED off"
+./led-hw5.sh off
+
+echo "Read LED status"
+./led-hw5.sh status
+
+echo "Turning LED flash"
+./led-hw5.sh flash
+
+echo "Turning LED blink 3 times"
+./led-hw5.sh blink 3
